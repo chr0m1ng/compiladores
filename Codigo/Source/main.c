a9246772a00a60b43cce429e20d242c9610cca58
Gabriel Santos <gabrielrsantoss@icloud.com>
2018-04-09 19:01:12 -0300

    Adicionado tabela de tokens e de palavras reservadas com suas respectivas funções

--- Codigo/Source/main.c
+++ Codigo/Source/main.c
@@ -1,15 +1,23 @@
-/* ------------------------------------------------------------- */
-/*                     ARQUIVO: main.c                           */
-/* ------------------------------------------------------------- */
+/*
+*---------------------------------------------------------------------
+*   INCLUDE FILES
+*---------------------------------------------------------------------
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 #include "../Header/transicoes.h"
+#include "../Header/tokens.h"
+#include "../Header/reservadas.h"
 
-int main(int argc, char *argv[]){
-    char simbolo = 'k';
-    printf("%u\n", tabela_transicao[20][charToAlfabeto(&simbolo)]);
+
+
+int main(int argc, char *argv[])
+{
+
+    printf("%s\n", tokenGetText( isPalavraReservada("fim_programa") ) );
+
     return 0;
 }
